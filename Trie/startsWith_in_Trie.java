93c4025dd674f002a731ea13eb4dc067315a3a28
Balla Kavya <ballakavya594@gmail.com>
2024-12-17 12:30:43 +0530

    Startswith in trie(prefix)

--- Trie/startsWith_in_Trie.java
+++ Trie/startsWith_in_Trie.java
@@ -1,35 +1,55 @@
 package Trie;
 
 public class startsWith_in_Trie {
-	class node{
+	class node {
 		node children[];
 		boolean end_of_word;
-		node(){
-			children=new node[26];
-			for(int i=0;i<26;i++) {
-				children[i]=null;
+
+		node() {
+			children = new node[26];
+			for (int i = 0; i < 26; i++) {
+				children[i] = null;
 			}
-			end_of_word=false;
+			end_of_word = false;
 		}
 	}
-	
-	node root=new node();
+
+	node root = new node();
+
 	public void insert(String word) {
-		word=word.toLowerCase();
-		for(int i=0;i<word.length();i++) {
-			int idx=word.charAt(i)-'a';
-			if(root.children[idx]==null) {
-				root.children[idx]=new node();
+		node current=root;
+		word = word.toLowerCase();
+		for (int i = 0; i < word.length(); i++) {
+			int idx = word.charAt(i) - 'a';
+			if (current.children[idx] == null) {
+				current.children[idx] = new node();
 			}
-			if(i==word.length()-1) {
-				root.children[idx].end_of_word=true;
+			if (i == word.length() - 1) {
+				current.children[idx].end_of_word = true;
 			}
-			root=root.children[idx];
+			current = current.children[idx];
 		}
+	}
+
+	public boolean startsWith(String word) {
+		node current = root;
+		for (int i = 0; i < word.length(); i++) {
+			int idx = word.charAt(i) - 'a';
+			if (current.children[idx] == null) {
+				return false;
+			}
+			current = current.children[idx];
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
-
+		startsWith_in_Trie t = new startsWith_in_Trie();
+		String words[] = { "apple", "RadhaKrishna", "Amma", "Nanna" };
+		for (int i = 0; i < words.length; i++) {
+			t.insert(words[i]);
+		}
+		System.out.println(t.startsWith("app"));
 	}
 
 }
